083f8d50b61344a2b93365b6f3c05312cba3fed1
forresto <forrest@sembiki.com>
2015-01-11 15:12:53 -0500

    output.v3SourceMap

--- coffee.js
+++ coffee.js
@@ -3,5 +3,5 @@ var Coffee = require('coffee-script');
 exports.translate = function(load) {
   var output = Coffee.compile(load.source, {sourceMap:true});
   load.source = output.js;
-  load.metadata.sourceMap = output.sourceMap;
+  load.metadata.sourceMap = output.v3SourceMap;
 };
