bb9ff5a1a408cd5d545185ef91b0bc8c08f8cf45
forresto <forrest@sembiki.com>
2015-01-12 16:19:25 -0500

    working with window.CoffeeScript, jspm/npm#37

--- coffee.js
+++ coffee.js
@@ -1,7 +1,13 @@
-var Coffee = require('coffee-script');
+// Ideally, this would be a proper jspm dependency.
+// For now, using http://coffeescript.org/extras/coffee-script.js
+// var Coffee = require('coffee-script');
+var Coffee = window.CoffeeScript;
 
 exports.translate = function(load) {
-  var output = Coffee.compile(load.source, {sourceMap:true});
+  var output = Coffee.compile(load.source, {
+    sourceMap: true,
+    bare: true
+  });
   load.source = output.js;
   load.metadata.sourceMap = output.v3SourceMap;
 };
--- test.coffee
+++ test.coffee
@@ -1,7 +1,8 @@
 module.exports = class coffeeTest
   constructor: (@message) ->
   go: (count) ->
+    console.log 'go'
     out = ""
-    for i in [0..count]
+    for i in [1..count]
       out += "#{@message} #{i}! "
     return out
