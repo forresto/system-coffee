330166c7d63f192fffcfdc6852ef6b7c2656cdb8
forresto <forrest@sembiki.com>
2015-01-12 14:10:13 -0500

    reorg

--- test.js
+++
@@ -1,8 +0,0 @@
-System.import('./test.coffee!')
-  .then(function (test) {
-    var t = new test('Test');
-    document.body.append(t.message(3));
-  })
-  .catch(function (err) {
-    console.log('ERROR: ', err);
-  });
